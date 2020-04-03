01acae12481c56af52d4861c1cf7cb15f25a19db
Aybek Sultanov <aybek-sultanov@inbox.ru>
2020-04-03 19:15:09 +0600

    Параметр для запроса сообщений

--- WebChat/WebChat/ResourceParameters/MessageParameters.cs
+++ WebChat/WebChat/ResourceParameters/MessageParameters.cs
@@ -4,6 +4,7 @@ namespace WebChat.ResourceParameters
 {
     public class MessagesParameters
     {
+        public int? UserId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
